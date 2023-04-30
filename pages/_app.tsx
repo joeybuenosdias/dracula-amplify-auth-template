d4d1797ea0d112abbee4fed30c4aff15bae3ba1f
Joey Schrader <dev@joeyschrader.com>
2023-04-30 10:42:59 -0500

    Add amplify auth

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,10 @@
+import "@aws-amplify/ui-react/styles.css";
+import { Amplify } from "aws-amplify";
+import awsExports from "../src/aws-exports";
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+
+Amplify.configure({ ...awsExports, ssr: true })
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
