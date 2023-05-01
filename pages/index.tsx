3a0c5687766c534d5992190db3c51de40ade47c1
Joey Schrader <dev@joeyschrader.com>
2023-04-30 19:06:52 -0500

    Added skeleton component and import lint rule

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,12 @@
-import type { NextPage } from "next";
-import Head from "next/head";
-import Image from "next/image";
-import styles from "../styles/Home.module.css";
 import { withAuthenticator } from "@aws-amplify/ui-react";
 import type { WithAuthenticatorProps } from "@aws-amplify/ui-react";
+import type { NextPage } from "next";
+import { Github } from "src/features/github";
+import { Layout } from "src/components/layout";
+
+// Must return type rule
+// Import order rule
+// no unused var rule
 
 export function getServerSideProps() {
   const renderedAt = new Date();
@@ -23,6 +26,8 @@ export function getServerSideProps() {
 const Home: NextPage = ({ signOut, user }: WithAuthenticatorProps) => {
   return (
     <div style={{ padding: 50 }}>
+      <Layout />
+      <Github />
       <h1>Logged in as {user?.username}.</h1>
       <div>
         <button onClick={signOut}>Sign out</button>
---
+++ src/features/github/github.tsx
@@ -0,0 +1,5 @@
+export function Github() {
+    return (
+        <div>Github component</div>
+    );
+}
